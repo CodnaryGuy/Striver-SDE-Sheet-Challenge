9672f88885b7c4b5c39bb03f53072f9e9e2b645f
CodnaryGuy <74415240+CodnaryGuy@users.noreply.github.com>
2022-06-08 22:57:02 +0530

    Add files via upload

---
+++ 7-Rotate-Image.java
@@ -0,0 +1,41 @@
+/*Step1: Transpose of the matrix. (transposing means changing columns to rows and rows to columns)
+Step2: Reverse each row of the matrix.*/
+
+import java.util.*;
+class TUF {
+    static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+/*Time Complexity: O(N*N) + O(N*N).One O(N*N) for transposing the matrix and
+ the other for reversing the matrix.
+Space Complexity: O(1).*/
